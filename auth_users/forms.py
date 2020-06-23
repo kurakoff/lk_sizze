710e4a1204a2c74412269c315b056cbbc50c1e71
sergeyKuznetsov <you@example.com>
2020-06-23 14:30:32 +0300

    forgot for email done, TODO: email html

--- auth_users/forms.py
+++ auth_users/forms.py
@@ -3,6 +3,8 @@ from django import forms
 
 
 class CreateUserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
--- auth_users/urls.py
+++ auth_users/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import PasswordResetConfirmView
 from django.urls import path
 from .views import \
     LoginView, \
