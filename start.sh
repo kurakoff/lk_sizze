6c8e7d3ac47d0c878dc0d547717804e2c35dd418
root <root@321362-cu18705.tmweb.ru>
2020-06-25 17:06:55 +0000

    merge

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python manage.py migrate --settings=sizzy_lk.production_settings
-python manage.py compress --settings=sizzy_lk.production_settings
+python3 manage.py migrate --settings=sizzy_lk.production_settings
+python3 manage.py compress --settings=sizzy_lk.production_settings
 echo yes | ./manage.py collectstatic
-python manage.py runserver
+python3 manage.py runserver
 
 #service nginx start
 #uwsgi --ini uwsgi.ini
