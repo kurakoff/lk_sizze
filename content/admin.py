39a3f6d34e0cfcee5abd6f0fd76691ac16cfcece
sergeyKuznetsov <you@example.com>
2020-06-30 11:50:16 +0300

    related elements done

--- content/admin.py
+++ content/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
+import nested_admin
 from .models import (
     Project,
     Screen,
     Prototype,
-    GroupElements,
+    Category,
+    CategoryPrototype,
     Element,
 )
 
 
-class PrototypeAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
+class ElementInline(nested_admin.NestedStackedInline):
+    model = Element
+    extra = 0
+
+
+class CategoryPrototypeInline(nested_admin.NestedStackedInline):
+    model = CategoryPrototype
+    inlines = [ElementInline]
+    extra = 0
+
+
+class PrototypeAdmin(nested_admin.NestedModelAdmin):
+    inlines = [CategoryPrototypeInline]
+
+
 admin.site.register(Prototype, PrototypeAdmin)
+admin.site.register(Category, CategoryAdmin)
