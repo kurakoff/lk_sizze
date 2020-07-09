8d03f699f40df9792cc7b636ee5c9e23dae66d4f
sergeyKuznetsov <you@example.com>
2020-07-09 22:22:35 +0300

    first

--- content/static/js/redactor/img.js
+++ content/static/js/redactor/img.js
@@ -28,19 +28,36 @@ $(document).ready(function () {
             $('.main-svg').css('transform', `scale(0.5)`);
         }
 
-        $('[data-set="true"]').on('click', (event) => {
+        // $('[data-set="true"]').on('click', (event) => {
+        //     let clickCounter = +$(event.target).attr('clickCounter');
+        //     if (clickCounter == 1) return;
+        //
+        //     editableHandler(event);
+        // });
+
+        $(document).on('click', '[data-set="true"]', function (event) {
+            console.log('on [data-set="true"]', $(event.target))
             let clickCounter = +$(event.target).attr('clickCounter');
             if (clickCounter == 1) return;
-
             editableHandler(event);
-        });
-        $('[data-set="true"]').on('click', getToolsPanel);
+        })
+
+        // $(document).on('click', '', function (event) {
+        // })
+
+        // $('[data-set="true"]').on('click', getToolsPanel);
+
+        $(document).on('click', '[data-set="true"]', getToolsPanel)
+
+
         $('.draggable').css('cursor', 'move');
 
         $('[data-type="text"]').css('line-height', 'normal');
         $('[data-type="text"]').attr('clickCounter', 0);
 
-        $('[data-type="text"]').on('click', (event) => {
+        $(document).on('click', '[data-type="text"]', function (event) {
+            console.log('on click [data-type="text"]', $(event.target))
+
             let clickCounter = +$(event.target).attr('clickCounter');
             if (clickCounter === 1) {
                 draggable.draggable = false;
@@ -50,9 +67,23 @@ $(document).ready(function () {
                 clickCounter++;
                 $(event.target).attr('clickCounter', clickCounter);
             }
-        });
+        })
+        //
+        // $('[data-type="text"]').on('click', (event) => {
+        //     let clickCounter = +$(event.target).attr('clickCounter');
+        //     if (clickCounter === 1) {
+        //         draggable.draggable = false;
+        //         draggable.snappable = false;
+        //         $(event.target).css('cursor', 'text');
+        //     } else {
+        //         clickCounter++;
+        //         $(event.target).attr('clickCounter', clickCounter);
+        //     }
+        // });
 
-        $('[data-type="text"]').blur((event) => {
+        $(document).on('blur', '[data-type="text"]', function (event) {
+
+            console.log('on click blur  [data-type="text"]', $(event.target))
             let clickCounter = +$(event.target).attr('clickCounter');
 
             if (clickCounter == 1) {
@@ -62,6 +93,18 @@ $(document).ready(function () {
                 $(event.target).attr('clickCounter', 0);
             }
         });
+
+        // $('[data-type="text"]').blur((event) => {
+        //     let clickCounter = +$(event.target).attr('clickCounter');
+        //
+        //     if (clickCounter == 1) {
+        //         draggable.draggable = true;
+        //         draggable.snappable = true;
+        //         $(event.target).css('cursor', 'move');
+        //         $(event.target).attr('clickCounter', 0);
+        //     }
+        // });
+        //
         $('.main-svg').contextmenu((event) => {
             $('.contextmenu').css('display', 'inline-block');
             $('.contextmenu').css('left', event.pageX);
