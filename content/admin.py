cf9da1b83156f1b4567b5f2b8a1384b9cc27519d
sergeyKuznetsov <you@example.com>
2020-07-17 18:50:37 +0300

    add readonly slug in settings

--- content/admin.py
+++ content/admin.py
@@ -20,6 +20,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class SettingsAdmin(admin.ModelAdmin):
     list_display = ('slug', 'value')
+    readonly_fields = ('slug',)
 
     def has_delete_permission(self, request, obj=None):
         return False
