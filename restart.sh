1d3885c9178465a92b40754445c2d7601f187d47
Sergey <sergey@321362-cu18705.tmweb.ru>
2020-07-20 15:18:03 +0000

    merge

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html/lk_sizze/ || exit
+fuser -k 8000/tcp
 git pull
 . venv/bin/activate
-fuser -k 8000/tcp
 echo yes | ./manage.py collectstatic
 nohup python manage.py runserver &
