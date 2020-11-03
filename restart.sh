cdc094371eb4da8e3c7e12a5b3ba935e116a9f03
dkrussia <Exos93009!!>
2020-11-03 14:59:13 +0300

    fix restart script

--- restart.sh
+++ restart.sh
@@ -3,5 +3,6 @@ cd /var/www/html/lk_sizze/ || exit
 fuser -k 8000/tcp
 git pull
 . venv/bin/activate
+python3 manage.py migrate --settings=sizzy_lk.production_settings
 echo yes | ./manage.py collectstatic
 nohup python manage.py runserver &
