981aef29e877cea8473093ca8ecc97f64e4d2f79
kabu1zhan <kabiljanz0301@gmail.com>
2021-02-26 16:09:04 +0600

    Add list all users v6

--- api_redactor_app/api.py
+++ api_redactor_app/api.py
@@ -165,8 +165,8 @@ class ProjectApiView(APIView):
                 Q(share_project__to_user=request.user.email) |
                 Q(SharedProject__all_users=True)
             )
-            project = Project.objects.filter(Q(user=request.user) | Q(share_project__to_user=request.user.email)
-                                             | Q(share_project__all_users="True")).all()
+            project = Project.objects.filter(Q(user=request.user) | Q(share_project__to_user__in=
+                                                                      [request.user.email, None])).all()
             serializer = ProjectSerializer(project, many=True)
 
         return JsonResponse({'project': serializer.data})
