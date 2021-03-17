d3b2ead2f23b9ab6a2cb19a3c63719d5d26b3a7c
kabu1zhan <kabiljanz0301@gmail.com>
2021-03-17 09:36:36 +0600

    Add data

--- sizzy_lk/settings.py
+++ sizzy_lk/settings.py
@@ -210,6 +210,11 @@ CRONJOBS = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
