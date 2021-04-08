eff26c55481c4aed06e7bee7beb9da653b81e714
kabu1zhan <kabiljanz0301@gmail.com>
2021-04-08 13:59:03 +0600

    CHange email post registration

--- api_redactor_app/api.py
+++ api_redactor_app/api.py
@@ -582,16 +582,15 @@ class ShareProjectAllView(APIView):
         if request.data.get('to_user') == request.user.email:
             return JsonResponse({'result': False, 'message': "You cant share the project with yourself"})
         serializer.save(project_id=kwargs['project_id'], from_user=request.user)
-        link = self.generate_link(kwargs['project_id'])
+        # link = self.generate_link(kwargs['project_id'])
         msg_html = render_to_string('mail/Share.html', {'to_user': serializer.data['to_user'],
                                                         'from_user': serializer.data['from_user'],
-                                                        'project': serializer.data['project'],
-                                                        'link': link
+                                                        'project': serializer.data['project']
                                                         })
         from auth_users.utils import send_html_mail
         send_html_mail(subject="Shared project with you", html_content=msg_html, sender=getattr(settings, "EMAIL_HOST_USER"),
                        recipient_list=[serializer.data['to_user']])
-        serializer.data['link'] = link
+        # serializer.data['link'] = link
         # send_mail(
         #     f"Shared project with you",
         #     msg_html,
