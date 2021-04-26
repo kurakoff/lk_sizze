bc849daaf87b12ec6e08ec912ab4d7fe29d4f2f5
kabu1zhan <kabiljanz0301@gmail.com>
2021-04-26 17:23:31 +0600

    Try fix bug token v23

--- api_redactor_app/middleware/cors_middleware.py
+++ api_redactor_app/middleware/cors_middleware.py
@@ -15,17 +15,17 @@ def open_access_middleware(get_response):
 def check_token_middleware(get_response):
     def middleware(request):
         response = get_response(request)
-        # try:
-        token = request.META.get("HTTP_AUTHORIZATION")
-        token_list = []
-        token = token.split()
-        back_token = Token.objects.all().values('key')
-        for i in back_token:
-            token_list.append(i['key'])
-        if token[1] not in token_list:
-            print('Tam')
-            return redirect("https://dashboard.sizze.io/sign-in")
+        # # try:
+        # token = request.META.get("HTTP_AUTHORIZATION")
+        # token_list = []
+        # token = token.split()
+        # back_token = Token.objects.all().values('key')
+        # for i in back_token:
+        #     token_list.append(i['key'])
+        # if token[1] not in token_list:
+        #     print('Tam')
+        return redirect("https://dashboard.sizze.io/sign-in")
         # except:
         #     pass
-        return response
+        # return response
     return middleware
