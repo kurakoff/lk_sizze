7c086d292e98976d6d7240ae8e4eb762cfa2a3d1
kabu1zhan <kabiljanz0301@gmail.com>
2021-05-04 19:21:27 +0600

    Add webhook

--- stripe_pay/api.py
+++ stripe_pay/api.py
@@ -32,3 +32,43 @@ class StripeApi(APIView):
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': {'message': str(e)}}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StripeWebhook(APIView):
+    def post(self, request):
+        webhook_secret = config('stripe_webhook')
+        request_data = json.loads(request.body)
+        if webhook_secret:
+            # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+            signature = request.headers.get('stripe-signature')
+            try:
+                event = stripe.Webhook.construct_event(
+                    payload=request.data, sig_header=signature, secret=webhook_secret)
+                data = event['data']
+            except Exception as e:
+                return e
+            # Get the type of webhook event sent - used to check the status of PaymentIntents.
+            event_type = event['type']
+        else:
+            data = request_data['data']
+            event_type = request_data['type']
+        data_object = data['object']
+
+        if event_type == 'checkout.session.completed':
+            # Payment is successful and the subscription is created.
+            # You should provision the subscription and save the customer ID to your database.
+            print(data)
+        elif event_type == 'invoice.paid':
+            # Continue to provision the subscription as payments continue to be made.
+            # Store the status in your database and check when a user accesses your service.
+            # This approach helps you avoid hitting rate limits.
+            print(data)
+        elif event_type == 'invoice.payment_failed':
+            # The payment failed or the customer does not have a valid payment method.
+            # The subscription becomes past_due. Notify your customer and send them to the
+            # customer portal to update their payment information.
+            print(data)
+        else:
+            print('Unhandled event type {}'.format(event_type))
+        return JsonResponse({'status': 'success'})
+
--- stripe_pay/urls.py
+++ stripe_pay/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('create-checkout-session/', api.StripeApi.as_view()),
+    path('webhook/', api.StripeApi.as_view())
 ]
