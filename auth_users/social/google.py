2b478aaa222a675d7be27c077f96804cecf4e401
kabu1zhan <kabiljanz0301@gmail.com>
2021-05-06 21:22:43 +0600

    Fix error 1

--- auth_users/social/google.py
+++ auth_users/social/google.py
@@ -3,13 +3,8 @@ from google.oauth2 import id_token
 
 
 class Google:
-    """Google class to fetch the user info and return it"""
-
     @staticmethod
     def validate(auth_token):
-        """
-        validate method Queries the Google oAUTH2 api to fetch the user info
-        """
         try:
             idinfo = id_token.verify_oauth2_token(
                 auth_token, requests.Request())
