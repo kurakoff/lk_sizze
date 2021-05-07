7daa8115ca14c42e25c0b263364eb74c7e5b745a
kabu1zhan <kabiljanz0301@gmail.com>
2021-05-07 20:11:10 +0600

    Add client portal v2

--- stripe_pay/urls.py
+++ stripe_pay/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('create-checkout-session/', api.StripeApi.as_view()),
     path('webhook/', api.StripeWebhook.as_view()),
-    path('customer-portal/')
+    path('customer-portal/', api.ClientPortal.as_view())
 ]
