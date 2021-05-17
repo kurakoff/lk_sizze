1f8d40ca18708916b2e2dd41ef3dc777b3b614c7
kabu1zhan <kabiljanz0301@gmail.com>
2021-05-17 23:20:47 +0600

    add perm

--- stripe_pay/api.py
+++ stripe_pay/api.py
@@ -122,12 +122,12 @@ class StripeWebhook(APIView):
         elif event_type == 'customer.subscription.created':
             client = ClientStrip.objects.get(client=data_object['customer'])
             plan = Price.objects.get(price=data_object['plan']['id'])
-            try:
-                print('start_try_new_sub')
-                past_sub = Subscription.objects.get(customer=client)
-                get_sub = stripe.Subscription.retrieve(id=past_sub.subscription)
-                print(get_sub)
-            except: pass
+            # try:
+            print('start_try_new_sub')
+            past_sub = Subscription.objects.get(customer=client)
+            get_sub = stripe.Subscription.retrieve(id=past_sub.subscription)
+            print(get_sub)
+            # except: pass
             Subscription.objects.create(
                 subscription=data_object['id'],
                 plan=plan,
