edf8e50e728960dbbb9e8634cb1ca2244b799fbc
kabu1zhan <kabiljanz0301@gmail.com>
2021-07-01 20:58:46 +0600

    delete comments

--- auth_users/auth_helpers/helpers.py
+++ auth_users/auth_helpers/helpers.py
@@ -17,15 +17,8 @@ class EmailBackend(ModelBackend):
 
 
 class TokenAuthSupportCookie(TokenAuthentication):
-    """
-    Extend the TokenAuthentication class to support cookie based authentication
-    """
     def authenticate(self, request):
-        # Check if 'auth_token' is in the request cookies.
-        # Give precedence to 'Authorization' header.
         if 'access_token' in request.COOKIES:
-            # and \
-            #         'HTTP_AUTHORIZATION' not in request.META
             return self.authenticate_credentials(
                 request.COOKIES.get('access_token')
             )
