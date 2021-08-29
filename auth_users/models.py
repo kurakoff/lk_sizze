d60bafa7de0055a83de2d13400a2e89c57a71306
kabu1zhan <kabiljanz0301@gmail.com>
2021-08-29 18:41:10 +0600

    fix

--- auth_users/models.py
+++ auth_users/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-User._meta.get_field('email')._unique = True
+# User._meta.get_field('email')._unique = True
 User._meta.get_field('username')._unique = False
